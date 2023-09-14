e3b641b46c8e4888c6b35bcdb34a0b684a643f28
Neftaly <neftalyrv23@gmail.com>
2023-09-14 19:55:36 -0400

    Change how verify from array to if statements

--- src/utils.js
+++ src/utils.js
@@ -1,17 +1,47 @@
 const isBottomEdgeSquare = (index) => {
-  const bottomEdgeSquares = [0, 8, 16, 24, 32, 40, 48, 56]
-  return bottomEdgeSquares.includes(index)
+  if (
+    index === 0 ||
+    index === 8 ||
+    index === 16 ||
+    index === 24 ||
+    index === 32 ||
+    index === 40 ||
+    index === 48 ||
+    index === 56
+  )
+    return true
+
+  return false
 }
 
 const isRightEdgeSquare = (index) => {
-  const leftEdgeSquares = [56, 57, 58, 59, 60, 61, 62, 63]
+  if (
+    index === 56 ||
+    index === 57 ||
+    index === 58 ||
+    index === 59 ||
+    index === 60 ||
+    index === 61 ||
+    index === 62 ||
+    index === 63
+  )
+    return true
 
-  return leftEdgeSquares.includes(index)
+  return false
 }
 
 const isUpperEdgeSquare = (index) => {
-  const upperEdgeSquares = [7, 15, 23, 31, 39, 47, 55]
+  if (
+    index === 7 ||
+    index === 15 ||
+    index === 23 ||
+    index === 31 ||
+    index === 39 ||
+    index === 47 ||
+    index === 55
+  )
+    return true
 
-  return upperEdgeSquares.includes(index)
+  return false
 }
 export { isBottomEdgeSquare, isRightEdgeSquare, isUpperEdgeSquare }
