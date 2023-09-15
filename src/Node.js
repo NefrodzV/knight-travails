fd6cd66b6f58211f3e48952dfee2b8aca2fd4406
Neftaly <neftalyrv23@gmail.com>
2023-09-15 19:44:21 -0400

    changed the class to be exported

--- src/Node.js
+++ src/Node.js
@@ -1,6 +1,6 @@
 // Implement this correctly
 // The data can be an object to store the piece object
-class Node {
+export default class Node {
   constructor(data) {
     this.data = data
     this.next = null
