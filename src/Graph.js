407415f19aa487259e055cba0409e414e07e5aa3
Neftaly <neftalyrv23@gmail.com>
2023-09-15 19:47:57 -0400

    updated print to the data in the src

--- src/Graph.js
+++ src/Graph.js
@@ -1,5 +1,7 @@
 // A graph implemented as a adjacency list
 
+import squaresMap from "./squaresMap"
+
 export default class Graph {
   constructor(v) {
     // Number of vertices in the graph
@@ -14,26 +16,28 @@ export default class Graph {
   }
 
   addEdge(v, w) {
-    this.adjacencyList.get(v).push(w)
+    const squareV = squaresMap.get(v)
+    const squareW = squaresMap.get(w)
+    this.adjacencyList.get(v).push(squareW)
 
     // Since the graph is sparse or undirected we can push the other
-    this.adjacencyList.get(w).push(v)
+    this.adjacencyList.get(w).push(squareV)
   }
 
   printGraph() {
     // get all the vertices
-    var get_keys = this.AdjList.keys()
+    var get_keys = this.adjacencyList.keys()
 
     // iterate over the vertices
     for (var i of get_keys) {
       // get the corresponding adjacency list
       // for the vertex
-      var get_values = this.AdjList.get(i)
+      var get_values = this.adjacencyList.get(i)
       var conc = ""
 
       // iterate over the adjacency list
       // concatenate the values into a string
-      for (var j of get_values) conc += j + " "
+      for (var j of get_values) conc += j.data.symbol + " "
 
       // print the vertex and its adjacency list
       console.log(i + " -> " + conc)
