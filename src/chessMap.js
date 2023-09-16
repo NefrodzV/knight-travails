d6b483215efee4c45f15da71802611a7d59b87ea
Neftaly <neftalyrv23@gmail.com>
2023-09-16 18:52:15 -0400

    fixed typo

--- src/chessMap.js
+++ src/chessMap.js
@@ -3,7 +3,7 @@ import Node from "./Node"
 const chessMap = new Map()
 
 // Generates the data for the chess map
-const symbols = ["a", "b,", "c", "d", "e", "f", "g", "h"]
+const symbols = ["a", "b", "c", "d", "e", "f", "g", "h"]
 for (let x = 0; x < 8; x++) {
   for (let y = 0; y < 8; y++) {
     const symbol = `${symbols[x]}${y + 1}`
