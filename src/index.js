e49423c4b16cd2b285ea2f5408e0e35ab83e2f11
Neftaly <neftalyrv23@gmail.com>
2023-09-18 21:46:21 -0400

    refactoring

--- src/index.js
+++ src/index.js
@@ -1,27 +1,4 @@
 import "./style.css"
-import {
-  isBottomEdgeSquare,
-  isRightEdgeSquare,
-  isUpperEdgeSquare,
-} from "./utils"
+import { getViableKnightMoves } from "./utils"
 import { knightGraph } from "./knightGraph"
 import { chessMap } from "./chessMap"
-console.log("Hello webpack")
-// chessboardGraph.printGraph()
-// console.log(squaresMap.get(0).data)
-// chessboardGraph.knightbfs(squaresMap.get(0), squaresMap.get(10))
-// chessboardGraph.bfs(squaresMap.get(0))
-// console.log(chessboardGraph)
-
-// knightGraph.printGraph()
-console.log(chessMap)
-
-// const symbols = ["a", "b,", "c", "d", "e", "f", "g", "h"]
-//
-// for (let x = 0; x < 8; x++) {
-//   console.log(`${symbols[x]}${x + 1}`)
-//   for (let y = 0; y < 8; y++) {
-//     console.log(`${x}${y}`)
-//     console.log(`x:${x} y:${y}`)
-//   }
-// }
